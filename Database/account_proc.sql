50f56ff867f3ab9c3fefbc67a507c4944fc800d9
Nguyễn Văn Nhật <1612457@student.hcmus.edu.vn>
2019-12-03 14:25:39 +0700

    Đăng nhập, đăng kí + một vài API

---
+++ Database/account_proc.sql
@@ -0,0 +1,81 @@
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE GetAccountByUsername(in us varchar(50))
+BEGIN
+	select * from account where username COLLATE utf8mb4_unicode_ci = us;
+END;$$
+DELIMITER ;
+
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE GetAccountByEmail(in e varchar(100))
+BEGIN
+	select * from account where email COLLATE utf8mb4_unicode_ci = e;
+END;$$
+DELIMITER ;
+
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE GetAccountByPhone(in phone varchar(15))
+BEGIN
+	select * from account where sdt COLLATE utf8mb4_unicode_ci = phone;
+END;$$
+DELIMITER ;
+
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE AddStudent(in username varchar(50), in password varchar(100), in hoten varchar(100),
+							in email varchar(100), in ngaysinh date, in gioitinh varchar(5), in diachi varchar(255),
+                            in thanhpho varchar(100), in sdt varchar(15),in chuoixacthuc varchar(255))
+BEGIN
+	insert into account values (null, username,password, hoten, email, ngaysinh, gioitinh, 1, diachi,thanhpho, sdt, 'active', null, '','', 1, 0, false, chuoixacthuc);
+END;$$
+DELIMITER ;
+
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE AddTeacher(in username varchar(50), in password varchar(100), in hoten varchar(100),
+							in email varchar(100), in ngaysinh date, in gioitinh varchar(5), in diachi varchar(255),
+                            in thanhpho varchar(100), in sdt varchar(15), in baigioithieu text, in monhoc varchar(255), in chuyennganh int(11), in tienday int(11),in chuoixacthuc varchar(255))
+BEGIN
+	insert into account values (null, username,password, hoten, email, ngaysinh, gioitinh, 2, diachi, thanhpho, sdt, 'active', null, baigioithieu,monhoc, chuyennganh, tienday, false, chuoixacthuc);
+END;$$
+DELIMITER ;
+
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE AddAdmin(in username varchar(50), in password varchar(100), in hoten varchar(100),
+							in email varchar(100), in ngaysinh date, in gioitinh varchar(5), in diachi varchar(255),
+                            in thanhpho varchar(100), in sdt varchar(15))
+BEGIN
+	insert into account values (null, username,password, hoten, email, ngaysinh, gioitinh, 3, diachi,thanhpho, sdt, 'active', null, '','', 1, 0, true,'');
+END;$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE GetAll_ChuyenNganh()
+BEGIN
+	select * from chuyennganh;
+END;$$
+DELIMITER ;
+
+
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE GetAccountVerify(in verify varchar(255))
+BEGIN	
+        select * from account where chuoixacthuc = verify;     
+END;$$
+DELIMITER ;
+
+DELIMITER $$
+USE `sql12314047`$$
+CREATE PROCEDURE UpdateAccountVerify(in i int(11))
+BEGIN	
+        update account set xacthuc = true where id = i;     
+END;$$
+DELIMITER ;
+
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const passport = require('passport')
+const passport = require('passport');
+var accountModel = require('../model/account.model');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -13,5 +14,82 @@ router.get('/google',passport.authenticate('google',{
 router.get('/google/callback', passport.authenticate('google',{successRedirect:'/home',
 failureRedirect:'/'
 }))
+
+
+router.post('/login', passport.authenticate('local-login', {
+  failureRedirect: '/failed',
+  successRedirect: '/loginsuccess',
+  failureFlash: true
+}),
+  function (req, res) {
+    if (req.body.remember) {
+      req.session.cookie.maxAge = 1000 * 60 * 3;
+    }
+    else {
+      req.session.cookie.expires = false;
+    }
+    res.redirect('/');
+  }
+);
+
+
+//router dang ky
+router.post('/register', passport.authenticate('local-signup', {
+  failureRedirect: '/failed',
+  successRedirect: '/success',
+  failureFlash: true
+}),
+  function (req, res) {}
+);
+
+router.post('/verify', function(req, res, next){
+  console.log(req.body.verify);
+  accountModel.getAccountVerify(req.body.verify).then(r=>{
+    console.log(r[0]);
+    if(r.length){
+      accountModel.updateAccountVerify(r[0].id).then(r1=>{
+        res.redirect('/success');
+      }).catch(err=>{
+        console.log(err);
+        req.flash('accountMsg', 'Lỗi khi xác thực');
+        res.redirect('/failed');
+      })
+    }
+    else{
+      req.flash('accountMsg', 'Xác thực không đúng.');
+      res.redirect('/failed');
+    }
+  }).catch(err=>{
+    console.log(err);
+    req.flash('accountMsg', 'Lỗi khi xác thực');
+    res.redirect('/failed');
+  })
+});
+
+router.get('/failed', function(req, res, next){
+  res.send(req.flash('accountMsg'));
+});
+router.get('/success', function(req, res, next){
+  res.send('Thành công');
+});
+router.get('/loginsuccess', function(req, res, next){
+  res.send(req.user);
+});
+
+router.get('/chuyennganh', function(req, res, next){
+  accountModel.getAllChuyenNganh().then(r=>{
+    if(r.length){
+      res.send(r);
+    }
+    else{
+      req.flash('accountMsg', 'Không có chuyên ngành nào.');
+      res.redirect('/failed');
+    }
+  }).catch(err=>{
+      console.log(err);
+      req.flash('accountMsg', 'Lỗi khi xác thực');
+      res.redirect('/failed');
+  })
+});
 
 module.exports = router;
